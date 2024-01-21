6314bb8ad7ec35cbdef148e25084e9d11eba9fc3
Sara Outhounjite <sara602samra@gmail.com>
2024-01-21 20:17:10 +0100

    Delete files

--- Front-end/src/app/Users.ts
+++
@@ -1,8 +0,0 @@
-export class Users {
-    id!:number;
-    emailId!:String;
-    userName!:String;
-    password!:String;
-    comments!: Comment[];
-    constructor(){}
-}
--- Front-end/src/app/users.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Users } from './Users';
-
-describe('Users', () => {
-  it('should create an instance', () => {
-    expect(new Users()).toBeTruthy();
-  });
-});
